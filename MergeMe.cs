da584781c241d2a3782985b04838a3d6d4c8e04f
KaMoni <monika.mang@dataone.de>
2013-09-26 09:18:32 +0200

    initial version

---
+++ MergeMe.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitplay
+{
+    public class MergeMe
+    {
+        public string hello { get; set; }
+        public string goodbye { get; set; }
+        public string ciao { get; set; }
+    }
+}
