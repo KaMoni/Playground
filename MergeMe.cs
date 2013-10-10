30ca24ec7678491593c422026f32930dc0d32a6f
MonikaMang <monika.mag@dataone.de>
2013-10-10 09:39:46 +0200

    Update MergeMe.cs
    Mang

--- MergeMe.cs
+++ MergeMe.cs
@@ -4,7 +4,7 @@ namespace gitplay
 {
     public class MergeMe
     {
-        public string helloKaMoni { get; set; }
+        public string helloKaMoniMang { get; set; }
         public string goodbye { get; set; }
         public string ciao { get; set; }
 
